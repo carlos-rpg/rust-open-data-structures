5633437047a29a7e47820a200a3015c3cc3545f2
Carlos Guzman <carlos.rpg@gmail.com>
2024-12-04 21:24:57 +0100

    Implement Indexing

--- src/array_queue.rs
+++ src/array_queue.rs
@@ -1,4 +1,5 @@
 use std::usize;
+use std::ops::Index;
 
 pub struct ArrayQueue<T> {
     array: Vec<T>,
@@ -16,6 +17,18 @@ impl<T> ArrayQueue<T> {
     }
 }
 
+impl<T> Index<usize> for ArrayQueue<T> {
+    type Output = T;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        if index >= self.len {
+            panic!("Index out of bounds");
+        }
+        &self.array[(self.first + index) % self.len]
+    }
+}
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -27,5 +40,47 @@ mod tests {
         assert!(queue.array.is_empty());
         assert_eq!(queue.first, usize::MAX);
         assert_eq!(queue.len, 0);
+    }
+
+    #[test]
+    #[should_panic]
+    fn index_empty_queue() {
+        let queue: ArrayQueue<i32> = ArrayQueue::initialize();
+        queue[0];
+    }
+
+    #[test]
+    fn index_starts_at_zero() {
+        let queue = ArrayQueue {
+            array: vec!['a', 'b', 'c'],
+            first: 0,
+            len: 3,
+        };
+        assert_eq!(queue[0], 'a');
+        assert_eq!(queue[1], 'b');
+        assert_eq!(queue[2], 'c');
+    }
+
+    #[test]
+    fn index_starts_at_nonzero() {
+        let queue = ArrayQueue {
+            array: vec!['a', 'b', 'c'],
+            first: 1,
+            len: 3,
+        };
+        assert_eq!(queue[0], 'b');
+        assert_eq!(queue[1], 'c');
+        assert_eq!(queue[2], 'a');
+    }
+
+    #[test]
+    #[should_panic]
+    fn index_out_of_queue_bounds() {
+        let queue = ArrayQueue {
+            array: vec!['a', 'b', 'c', 'w', 'k'],
+            first: 2,
+            len: 3,
+        };
+        queue[3];
     }
 }
