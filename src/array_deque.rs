1d807a3ec18d7b6bf5237e199d82eee20a7de2f9
Carlos Guzman <carlos.rpg@gmail.com>
2024-12-14 17:26:19 +0100

    Implement ArrayDeque::get

--- src/array_deque.rs
+++ src/array_deque.rs
@@ -4,12 +4,45 @@ pub struct ArrayDeque<T> {
     len: usize,
 }
 
-impl<T> ArrayDeque<T> {
+#[derive(Debug, PartialEq)]
+pub enum Error {
+    IndexOutOfBounds(usize),
+}
+
+impl<T: Clone> ArrayDeque<T> {
     pub fn initialize() {
         Self { array: Vec::new(), first: 0, len: 0 }
     }
 
     pub fn len(&self) -> usize {
         self.len
+    }
+
+    pub fn get(&self, i: usize) -> Result<T, Error> {
+        if i < self.len() {
+            let target = (self.first + i) % self.array.len();
+            Ok(self.array[target].clone())
+        }
+        else {
+            Err(Error::IndexOutOfBounds(i))
+        }
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn get() {
+        let queue = ArrayDeque { 
+            array: vec!['a', 'b', 'c'],
+            first: 2,
+            len: 2,
+        };
+        assert_eq!(queue.get(0), Ok('c'));
+        assert_eq!(queue.get(1), Ok('a'));
+        assert_eq!(queue.get(2), Err(Error::IndexOutOfBounds(2)));
     }
 }
