e447fdce846b76cc43544d74adbfeb7b99f3fcbf
Carlos Guzman <carlos.rpg@gmail.com>
2024-12-14 17:52:28 +0100

    Implement ArrayDeque::set

--- src/array_deque.rs
+++ src/array_deque.rs
@@ -27,6 +27,13 @@ impl<T: Clone> ArrayDeque<T> {
             Err(Error::IndexOutOfBounds(i))
         }
     }
+
+    pub fn set(&mut self, i: usize, x: T) -> Result<T, Error> {
+        let y = self.get(i)?;
+        let target = (self.first + i) % self.array.len();
+        self.array[target] = x;
+        Ok(y)
+    }
 }
 
 
@@ -44,5 +51,32 @@ mod tests {
         assert_eq!(queue.get(0), Ok('c'));
         assert_eq!(queue.get(1), Ok('a'));
         assert_eq!(queue.get(2), Err(Error::IndexOutOfBounds(2)));
+    }
+
+    #[test]
+    fn set_check_output() {
+        let mut queue = ArrayDeque {
+            array: vec![1, 2, 3],
+            first: 1,
+            len: 2,
+        };
+        assert_eq!(queue.set(0, 20), Ok(2));
+        assert_eq!(queue.set(1, 30), Ok(3));
+        assert_eq!(queue.set(2, 10), Err(Error::IndexOutOfBounds(2)));
+    }
+
+    #[test]
+    fn set_check_array() {
+        let mut queue = ArrayDeque {
+            array: vec![1, 2, 3],
+            first: 1,
+            len: 2,
+        };
+        let _ = queue.set(0, 20);
+        assert_eq!(queue.array, vec![1, 20, 3]);
+        let _ = queue.set(1, 30);
+        assert_eq!(queue.array, vec![1, 20, 30]);
+        let _ = queue.set(2, 10);
+        assert_eq!(queue.array, vec![1, 20, 30]);
     }
 }
