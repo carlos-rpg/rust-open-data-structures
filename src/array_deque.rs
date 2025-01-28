d8366d86061a83aa097c7e14534ce8414886a670
Carlos Guzman <carlos.rpg@gmail.com>
2025-01-28 22:09:37 +0100

    Comment out `ArrayDeque::add()` related functionality

--- src/array_deque.rs
+++ src/array_deque.rs
@@ -39,25 +39,25 @@ impl<T: Clone> ArrayDeque<T> {
         Ok(y)
     }
 
-    pub fn add(&mut self, i: usize, x: T) {
-        if self.is_out_of_insert_bounds(i) {
-            panic!("Insertion index out of bounds");
-        }
-        if self.is_full() {
-            self.reset_array();
-            self.array.insert(i, x);
-        }
-        else {
-            for j in (i + 1..=self.len()).rev() {
-                let j_array = self.index_array(j);
-                let jm1_array = self.index_array(j - 1);
-                self.array[j_array] = self.array[jm1_array].clone();
-            }
-            let i_array = self.index_array(i);
-            self.array[i_array] = x;
-        }
-        self.len += 1;
-    }
+    // pub fn add(&mut self, i: usize, x: T) {
+    //     if self.is_out_of_insert_bounds(i) {
+    //         panic!("Insertion index out of bounds");
+    //     }
+    //     if self.is_full() {
+    //         self.reset_array();
+    //         self.array.insert(i, x);
+    //     }
+    //     else {
+    //         for j in (i + 1..=self.len()).rev() {
+    //             let j_array = self.index_array(j);
+    //             let jm1_array = self.index_array(j - 1);
+    //             self.array[j_array] = self.array[jm1_array].clone();
+    //         }
+    //         let i_array = self.index_array(i);
+    //         self.array[i_array] = x;
+    //     }
+    //     self.len += 1;
+    // }
 
     fn is_out_of_index_bounds(&self, i: usize) -> bool {
         i >= self.len()
@@ -177,82 +177,82 @@ mod tests {
         assert_eq!(queue.array, vec![1, 20, 30]);
     }
 
-    #[test]
-    fn add_as_push() {
-        let mut queue = ArrayDeque {
-            array: vec![],
-            head: 0,
-            len: 0,
-        };
-        queue.add(0, 'a');
-        queue.add(1, 'b');
-        queue.add(2, 'c');
-        assert_eq!(queue.array, vec!['a', 'b', 'c']);
-        assert_eq!(queue.head, 0);
-        assert_eq!(queue.len, 3);
-    }
+    // #[test]
+    // fn add_as_push() {
+    //     let mut queue = ArrayDeque {
+    //         array: vec![],
+    //         head: 0,
+    //         len: 0,
+    //     };
+    //     queue.add(0, 'a');
+    //     queue.add(1, 'b');
+    //     queue.add(2, 'c');
+    //     assert_eq!(queue.array, vec!['a', 'b', 'c']);
+    //     assert_eq!(queue.head, 0);
+    //     assert_eq!(queue.len, 3);
+    // }
 
-    #[test]
-    fn add_as_front_insertion() {
-        let mut queue = ArrayDeque {
-            array: vec![],
-            head: 0,
-            len: 0,
-        };
-        queue.add(0, 'a');
-        queue.add(0, 'b');
-        queue.add(0, 'c');
-        assert_eq!(queue.array, vec!['c', 'b', 'a']);
-        assert_eq!(queue.head, 0);
-        assert_eq!(queue.len, 3);
-    }
+    // #[test]
+    // fn add_as_front_insertion() {
+    //     let mut queue = ArrayDeque {
+    //         array: vec![],
+    //         head: 0,
+    //         len: 0,
+    //     };
+    //     queue.add(0, 'a');
+    //     queue.add(0, 'b');
+    //     queue.add(0, 'c');
+    //     assert_eq!(queue.array, vec!['c', 'b', 'a']);
+    //     assert_eq!(queue.head, 0);
+    //     assert_eq!(queue.len, 3);
+    // }
 
-    #[test]
-    fn add_within_array() {
-        let mut queue = ArrayDeque {
-            array: vec![1, 2, 3],
-            head: 1,
-            len: 2,
-        };
-        queue.add(0, 10);
-        assert_eq!(queue.array, vec![3, 10, 2]);
-        assert_eq!(queue.head, 1);
-        assert_eq!(queue.len, 3);
-        queue.add(1, 20);
-        assert_eq!(queue.array, vec![10, 20, 2, 3]);
-        assert_eq!(queue.head, 0);
-        assert_eq!(queue.len, 4);
-    }
+    // #[test]
+    // fn add_within_array() {
+    //     let mut queue = ArrayDeque {
+    //         array: vec![1, 2, 3],
+    //         head: 1,
+    //         len: 2,
+    //     };
+    //     queue.add(0, 10);
+    //     assert_eq!(queue.array, vec![3, 10, 2]);
+    //     assert_eq!(queue.head, 1);
+    //     assert_eq!(queue.len, 3);
+    //     queue.add(1, 20);
+    //     assert_eq!(queue.array, vec![10, 20, 2, 3]);
+    //     assert_eq!(queue.head, 0);
+    //     assert_eq!(queue.len, 4);
+    // }
 
-    #[test]
-    fn add_as_append() {
-        let mut queue = ArrayDeque {
-            array: vec![1, 2, 3],
-            head: 1,
-            len: 2,
-        };
-        queue.add(2, 10);
-        assert_eq!(queue.array, vec![10, 2, 3]);
-        assert_eq!(queue.head, 1);
-        assert_eq!(queue.len, 3);
-        queue.add(3, 20);
-        assert_eq!(queue.array, vec![2, 3, 10, 20]);
-        assert_eq!(queue.head, 0);
-        assert_eq!(queue.len, 4);
-        queue.add(4, 30);
-        assert_eq!(queue.array, vec![2, 3, 10, 20, 30]);
-        assert_eq!(queue.head, 0);
-        assert_eq!(queue.len, 5);
-    }
+    // #[test]
+    // fn add_as_append() {
+    //     let mut queue = ArrayDeque {
+    //         array: vec![1, 2, 3],
+    //         head: 1,
+    //         len: 2,
+    //     };
+    //     queue.add(2, 10);
+    //     assert_eq!(queue.array, vec![10, 2, 3]);
+    //     assert_eq!(queue.head, 1);
+    //     assert_eq!(queue.len, 3);
+    //     queue.add(3, 20);
+    //     assert_eq!(queue.array, vec![2, 3, 10, 20]);
+    //     assert_eq!(queue.head, 0);
+    //     assert_eq!(queue.len, 4);
+    //     queue.add(4, 30);
+    //     assert_eq!(queue.array, vec![2, 3, 10, 20, 30]);
+    //     assert_eq!(queue.head, 0);
+    //     assert_eq!(queue.len, 5);
+    // }
 
-    #[test]
-    #[should_panic]
-    fn add_out_of_bounds() {
-        let mut queue = ArrayDeque {
-            array: vec![1, 2, 3],
-            head: 1,
-            len: 2,
-        };
-        queue.add(3, 10);
-    }
+    // #[test]
+    // #[should_panic]
+    // fn add_out_of_bounds() {
+    //     let mut queue = ArrayDeque {
+    //         array: vec![1, 2, 3],
+    //         head: 1,
+    //         len: 2,
+    //     };
+    //     queue.add(3, 10);
+    // }
 }
