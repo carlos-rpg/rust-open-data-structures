a6b90ba7ecebd2b1d5df3077cd3d442f31a0bee6
Carlos Guzman <carlos.rpg@gmail.com>
2025-01-30 00:07:01 +0100

    Reimplement `ArrayDeque::add()`

--- src/array_deque.rs
+++ src/array_deque.rs
@@ -1,5 +1,6 @@
 use crate::circular_vec::CircularVec;
 
+#[derive(Debug)]
 pub struct ArrayDeque<T: Clone + PartialEq> {
     array: CircularVec<T>,
     len: usize,
@@ -38,25 +39,31 @@ impl<T: Clone + PartialEq> ArrayDeque<T> {
         Ok(y)
     }
 
-    // pub fn add(&mut self, i: usize, x: T) {
-    //     if self.is_out_of_insert_bounds(i) {
-    //         panic!("Insertion index out of bounds");
-    //     }
-    //     if self.is_full() {
-    //         self.reset_array();
-    //         self.array.insert(i, x);
-    //     }
-    //     else {
-    //         for j in (i + 1..=self.len()).rev() {
-    //             let j_array = self.index_array(j);
-    //             let jm1_array = self.index_array(j - 1);
-    //             self.array[j_array] = self.array[jm1_array].clone();
-    //         }
-    //         let i_array = self.index_array(i);
-    //         self.array[i_array] = x;
-    //     }
-    //     self.len += 1;
-    // }
+    pub fn add(&mut self, i: usize, x: T) -> Result<(), Error> {
+        if self.is_out_of_insert_bounds(i) {
+            Err(Error::IndexOutOfBounds(i))
+        }
+        else {
+            if self.is_full() {
+                self.array.resize((2 * self.len()).max(1), x.clone());
+            }
+            if i < self.len() / 2 {
+                self.array.shift_head(-1);
+
+                for j in 0..i {
+                    self.array[j] = self.array[j + 1].clone();
+                }
+            }
+            else {
+                for j in (i + 1..=self.len()).rev() {
+                    self.array[j] = self.array[j - 1].clone();
+                }
+            }
+            self.array[i] = x;
+            self.len += 1;
+            Ok(())
+        }
+    }
 
     fn is_out_of_index_bounds(&self, i: usize) -> bool {
         i >= self.len()
@@ -105,96 +112,70 @@ mod tests {
     }
 
     #[test]
-    fn set_check_array() {
-        let mut queue = ArrayDeque {
+    fn set_check_deque() {
+        let mut q1 = ArrayDeque {
             array: CircularVec::new(vec![1, 2, 3], 1),
             len: 2,
         };
-        let _ = queue.set(0, 20);
-        assert_eq!(queue.array, CircularVec::new(vec![1, 20, 3], 1));
-        let _ = queue.set(1, 30);
-        assert_eq!(queue.array, CircularVec::new(vec![1, 20, 30], 1));
-        let _ = queue.set(2, 10);
-        assert_eq!(queue.array, CircularVec::new(vec![1, 20, 30], 1));
+        let _ = q1.set(0, 20);
+        let _ = q1.set(1, 30);
+        let _ = q1.set(2, 10);
+
+        let q2 = ArrayDeque {
+            array: CircularVec::new(vec![1, 20, 30], 1),
+            len: 2,
+        };
+        assert_eq!(q1, q2);
+    }
+
+    #[test]
+    fn add_back_insertion() {
+        let mut q1 = ArrayDeque {
+            array: CircularVec::new(vec![], 0),
+            len: 0,
+        };
+        let _ = q1.add(0, 'a');
+        let _ = q1.add(1, 'b');
+        let _ = q1.add(2, 'c');
+
+        let q2 = ArrayDeque {
+            array: CircularVec::new(vec!['a', 'b', 'c'], 0),
+            len: 3,
+        };
+        assert_eq!(q1, q2);
+    }
+
+    #[test]
+    fn add_front_insertion() {
+        let mut q1 = ArrayDeque {
+            array: CircularVec::new(vec![], 0),
+            len: 0,
+        };
+        let _ = q1.add(0, 'a');
+        let _ = q1.add(0, 'b');
+        let _ = q1.add(0, 'c');
+
+        let q2 = ArrayDeque {
+            array: CircularVec::new(vec!['c', 'b', 'a'], 0),
+            len: 3,
+        };
+        assert_eq!(q1, q2);
     }
 
     // #[test]
-    // fn add_as_push() {
-    //     let mut queue = ArrayDeque {
-    //         array: vec![],
-    //         head: 0,
-    //         len: 0,
-    //     };
-    //     queue.add(0, 'a');
-    //     queue.add(1, 'b');
-    //     queue.add(2, 'c');
-    //     assert_eq!(queue.array, vec!['a', 'b', 'c']);
-    //     assert_eq!(queue.head, 0);
-    //     assert_eq!(queue.len, 3);
-    // }
-
-    // #[test]
-    // fn add_as_front_insertion() {
-    //     let mut queue = ArrayDeque {
-    //         array: vec![],
-    //         head: 0,
-    //         len: 0,
-    //     };
-    //     queue.add(0, 'a');
-    //     queue.add(0, 'b');
-    //     queue.add(0, 'c');
-    //     assert_eq!(queue.array, vec!['c', 'b', 'a']);
-    //     assert_eq!(queue.head, 0);
-    //     assert_eq!(queue.len, 3);
-    // }
-
-    // #[test]
-    // fn add_within_array() {
-    //     let mut queue = ArrayDeque {
-    //         array: vec![1, 2, 3],
-    //         head: 1,
+    // fn add_mid_insertion() {
+    //     let mut q1 = ArrayDeque {
+    //         array: CircularVec::new(vec![1, 2, 3], 1),
     //         len: 2,
     //     };
-    //     queue.add(0, 10);
-    //     assert_eq!(queue.array, vec![3, 10, 2]);
-    //     assert_eq!(queue.head, 1);
-    //     assert_eq!(queue.len, 3);
-    //     queue.add(1, 20);
-    //     assert_eq!(queue.array, vec![10, 20, 2, 3]);
-    //     assert_eq!(queue.head, 0);
-    //     assert_eq!(queue.len, 4);
-    // }
+    //     q1.add(0, 10);
+    //     q1.add(1, 20);
 
-    // #[test]
-    // fn add_as_append() {
-    //     let mut queue = ArrayDeque {
-    //         array: vec![1, 2, 3],
-    //         head: 1,
+    //     let mut q1 = ArrayDeque {
+    //         array: CircularVec::new(vec![1, 2, 3], 1),
     //         len: 2,
     //     };
-    //     queue.add(2, 10);
-    //     assert_eq!(queue.array, vec![10, 2, 3]);
-    //     assert_eq!(queue.head, 1);
-    //     assert_eq!(queue.len, 3);
-    //     queue.add(3, 20);
-    //     assert_eq!(queue.array, vec![2, 3, 10, 20]);
-    //     assert_eq!(queue.head, 0);
-    //     assert_eq!(queue.len, 4);
-    //     queue.add(4, 30);
-    //     assert_eq!(queue.array, vec![2, 3, 10, 20, 30]);
-    //     assert_eq!(queue.head, 0);
-    //     assert_eq!(queue.len, 5);
-    // }
-
-    // #[test]
-    // #[should_panic]
-    // fn add_out_of_bounds() {
-    //     let mut queue = ArrayDeque {
-    //         array: vec![1, 2, 3],
-    //         head: 1,
-    //         len: 2,
-    //     };
-    //     queue.add(3, 10);
+    //     assert_eq!(q1.array, vec![10, 20, 2, 3]);
     // }
 
     #[test]
