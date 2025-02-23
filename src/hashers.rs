f1c1c0fe503fad97b3baea6a8948b1208469d147
Carlos Guzman <carlos.rpg@gmail.com>
2025-02-23 10:25:34 +0100

    Add test for multiplicative hash outputs

--- src/hashers.rs
+++ src/hashers.rs
@@ -36,8 +36,7 @@ impl hash::Hasher for Multiplicative {
 
 #[cfg(test)]
 mod tests {
-    use std::hash::Hash;
-
+    use std::hash::{Hash, Hasher};
     use super::*;
 
     #[test]
@@ -86,5 +85,44 @@ mod tests {
         assert_eq!(0.hash(&mut h3), 0.hash(&mut h3));
         assert_eq!(42.hash(&mut h3), 42.hash(&mut h3));
         assert_eq!(1234567890.hash(&mut h3), 1234567890.hash(&mut h3));
+    }
+
+    #[test]
+    fn hash_values() {
+        let mut h1 = Multiplicative {
+            state: 0,
+            odd: 17675664392375410501,
+            dim: 1,
+        };
+        769936456459913124_u64.hash(&mut h1);
+        assert_eq!(h1.finish(), 0);
+        4993990495206945374_u64.hash(&mut h1);
+        assert_eq!(h1.finish(), 1);
+        6909495363674708222_u64.hash(&mut h1);
+        assert_eq!(h1.finish(), 1);
+
+        let mut h2 = Multiplicative {
+            state: 0,
+            odd: 10886466572363013235,
+            dim: 32,
+        };
+        10168802271749888757_u64.hash(&mut h2);
+        assert_eq!(h2.finish(), 3310380457);
+        18339155737800036837_u64.hash(&mut h2);
+        assert_eq!(h2.finish(), 1773933754);
+        285347091100835473_u64.hash(&mut h2);
+        assert_eq!(h2.finish(), 453384951);
+
+        let mut h3 = Multiplicative {
+            state: 0,
+            odd: 1939403831449563455,
+            dim: 64,
+        };
+        15344511071369365520_u64.hash(&mut h3);
+        assert_eq!(h3.finish(), 12818618549666319344);
+        14518584061463575402_u64.hash(&mut h3);
+        assert_eq!(h3.finish(), 10276551606605506838);
+        15761423750684663989_u64.hash(&mut h3);
+        assert_eq!(h3.finish(), 18276548952320700811);
     }
 }
