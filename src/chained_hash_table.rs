fe3540af7662f4f240531f75306251c33e078c7b
Carlos Guzman <carlos.rpg@gmail.com>
2025-02-25 20:47:47 +0100

    Rewrite `chained_hash_table.rs` to conform to `hashers.rs`

--- src/chained_hash_table.rs
+++ src/chained_hash_table.rs
@@ -1,11 +1,11 @@
-use rand;
 use std::mem;
+use crate::hashers::DimHasher;
 
 #[derive(Debug, Clone)]
-pub struct ChainedHashTable {
+pub struct ChainedHashTable<H: DimHasher> {
     dim: u32,
-    table: Vec<Vec<u32>>,
-    odd: u32,
+    table: Vec<Vec<u64>>,
+    hasher: H,
     len: usize
 }
 
@@ -15,30 +15,30 @@ pub enum Error {
     KeyNotFound,
 }
 
-impl ChainedHashTable {
-    pub fn initialize(dim: u32) -> Self {
+impl<H: DimHasher> ChainedHashTable<H> {
+    pub fn initialize(dim: u32, hasher: H) -> Self {
         assert!(dim > 0, "ChainedHashTable dimension must be greater than 0");
         let table_len = 2usize.pow(dim);
         let table = vec![vec![]; table_len];
-        let odd = 2 * rand::random_range(u32::MIN..u32::MAX / 2) + 1;
-        Self { dim, table, odd, len: 0 }
+        Self { dim, table, hasher, len: 0 }
     }
 
     pub fn len(&self) -> usize {
         self.len
     }
 
-    pub fn hash(&self, x: u32) -> usize {
-        let y = self.odd.overflowing_mul(x).0 >> (u32::BITS - self.dim);
-        y.try_into().expect("Unable to cast x's type into usize")
+    pub fn hash(&self, x: u64) -> usize {
+        let y = self.hasher.hash(x, self.dim);
+        y.try_into().expect("Unable to cast x's u64 into usize")
     }
 
-    pub fn contains(&self, x: u32) -> bool {
-        let row = &self.table[self.hash(x)];
+    pub fn contains(&self, x: u64) -> bool {
+        let i = self.hash(x);
+        let row = &self.table[i];
         row.iter().any(|y| *y == x)
     }
 
-    pub fn add(&mut self, x: u32) -> Result<(), Error> {
+    pub fn add(&mut self, x: u64) -> Result<(), Error> {
         if self.contains(x) {
             Err(Error::KeyAlreadyExists)
         }
@@ -53,7 +53,7 @@ impl ChainedHashTable {
         }
     }
 
-    pub fn remove(&mut self, x: u32) -> Result<(), Error> {
+    pub fn remove(&mut self, x: u64) -> Result<(), Error> {
         let i = self.hash(x);
 
         let j = self.table[i].iter()
@@ -69,15 +69,16 @@ impl ChainedHashTable {
         Ok(())
     }
 
-    fn resize(&mut self, dim: u32) {
-        let mut other = Self::initialize(dim);
-        mem::swap(self, &mut other);
+    fn resize(&mut self, to_dim: u32) {
+        self.dim = to_dim;
+        let table_len = 2usize.pow(to_dim);
+        let mut table = vec![vec![]; table_len];
+        mem::swap(&mut self.table, &mut table);
 
-        for row in other.table {
+        for row in table {
             for x in row {
                 let i = self.hash(x);
                 self.table[i].push(x);
-                self.len += 1;
             }
         }
     }
@@ -91,9 +92,9 @@ impl ChainedHashTable {
     }
 }
 
-impl PartialEq for ChainedHashTable {
+impl<H: DimHasher> PartialEq for ChainedHashTable<H> {
     fn eq(&self, other: &Self) -> bool {
-        let is_in_other = |row: &Vec<u32>| row
+        let is_in_other = |row: &Vec<u64>| row
             .iter()
             .all(|x| other.contains(*x));
 
@@ -105,67 +106,28 @@ impl PartialEq for ChainedHashTable {
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    fn cht(dim: u32, table: Vec<Vec<u32>>, odd: u32, len: usize) -> ChainedHashTable {
-        ChainedHashTable { dim, table, odd, len }
-    }
+    use crate::hashers;
 
     #[test]
     fn initialize() {
-        let chs1 = ChainedHashTable::initialize(1);
+        let h1= hashers::Multiplicative::new();
+        let chs1 = ChainedHashTable::initialize(1, h1);
         assert_eq!(chs1.dim, 1);
         assert_eq!(chs1.table.len(), 2);
-        assert_eq!(chs1.odd % 2, 1);
         assert_eq!(chs1.len(), 0);
 
-        let chs2 = ChainedHashTable::initialize(2);
-        assert_eq!(chs2.dim, 2);
-        assert_eq!(chs2.table.len(), 4);
-        assert_eq!(chs2.odd % 2, 1);
+        let h2 = hashers::Multiplicative::new();
+        let chs2 = ChainedHashTable::initialize(10, h2);
+        assert_eq!(chs2.dim, 10);
+        assert_eq!(chs2.table.len(), 1024);
         assert_eq!(chs2.len(), 0);
     }
 
     #[test]
     #[should_panic]
-    fn initialize_wrong_dim() {
-        let _chs = ChainedHashTable::initialize(0);
-    }
-
-    #[test]
-    fn hash() {
-        let cht1 = cht(1, vec![], 2579419223, 0);
-        assert_eq!(cht1.hash(790641268), 1);
-        assert_eq!(cht1.hash(2204740408), 0);
-        assert_eq!(cht1.hash(3442113750), 1);
-
-        let cht2 = cht(16, vec![], 1881984275, 0);
-        assert_eq!(cht2.hash(3783082547), 47890);
-        assert_eq!(cht2.hash(103666855), 50532);
-        assert_eq!(cht2.hash(2941348170), 44307);
-
-        let cht3 = cht(32, vec![], 694784213, 0);
-        assert_eq!(cht3.hash(3066815969), 457036853);
-        assert_eq!(cht3.hash(59612175), 96618619);
-        assert_eq!(cht3.hash(3151035214), 1664808934);
-    }
-
-    #[test]
-    fn contains() {
-        let cht1 = cht(
-            2, 
-            vec![
-                vec![3618622750],
-                vec![2688180142, 3783518487],
-                vec![],
-                vec![3826048670],
-            ],
-            49460819,
-            4,
-        );
-        assert!(cht1.contains(3618622750));
-        assert!(cht1.contains(3783518487));
-        assert!(cht1.contains(3826048670));
-        assert!(!cht1.contains(42))
+    fn initialize_dim_zero() {
+        let h = hashers::Multiplicative::new();
+        let _ = ChainedHashTable::initialize(0, h);
     }
 
     #[test]
@@ -173,31 +135,31 @@ mod tests {
         let cht1 = ChainedHashTable {
             dim: 1, 
             table: vec![
-                vec![2450321026],
-                vec![1985059619, 2014097083, 3473442836],
+                vec![0, 1013],
+                vec![3, 43, 18446744073709551615],
             ],
-            odd: 1059656881,
-            len: 4,
+            hasher: hashers::Multiplicative::with_seed(42),
+            len: 5,
         };
         let cht2 = ChainedHashTable {
             dim: 2, 
             table: vec![
-                vec![1985059619],
-                vec![2450321026, 2014097083],
-                vec![3473442836],
-                vec![],
+                vec![3, 0],
+                vec![18446744073709551615],
+                vec![43],
+                vec![1013],
             ],
-            odd: 3366355585,
-            len: 4,
+            hasher: hashers::Multiplicative::with_seed(7),
+            len: 5,
         };
         let cht3 = ChainedHashTable {
             dim: 1, 
             table: vec![
-                vec![0],
-                vec![2450321026, 1985059619, 3473442836],
+                vec![0, 18446744073709551615],
+                vec![3, 10, 2026],
             ],
-            odd: 949054937,
-            len: 4,
+            hasher: hashers::Multiplicative::with_seed(13),
+            len: 5,
         };
         assert_eq!(cht1, cht2);
         assert_ne!(cht1, cht3);
@@ -209,65 +171,38 @@ mod tests {
         let cht1 = ChainedHashTable {
             dim: 1, 
             table: vec![
-                vec![2450321026],
-                vec![1985059619, 2014097083],
+                vec![0],
+                vec![2450321026, 18446744073709551615],
             ],
-            odd: 1059656881,
+            hasher: hashers::Multiplicative::with_seed(555),
             len: 3,
         };
         let cht2 = ChainedHashTable {
             dim: 2, 
             table: vec![
-                vec![1985059619],
-                vec![2450321026, 2014097083],
-                vec![3473442836],
+                vec![0, 1985059619],
+                vec![18446744073709551615],
+                vec![2450321026],
                 vec![],
             ],
-            odd: 3366355585,
+            hasher: hashers::Multiplicative::with_seed(777),
             len: 4,
         };
         assert_ne!(cht1, cht2);
     }
-
-    #[test]
-    fn resize() {
-        let cht1 = ChainedHashTable {
-            dim: 1, 
-            table: vec![
-                vec![425364276, 3151484320, 4161335175, 3053504173, 1496293961],
-                vec![3199143396, 3057340300, 3235619151],
-            ],
-            odd: 1610605069,
-            len: 8,
-        };
-        let mut cht2 = cht1.clone();
-
-        cht2.resize(2);
-        assert_eq!(cht1, cht2);
-        cht2.resize(3);
-        assert_eq!(cht1, cht2);
-        cht2.resize(4);
-        assert_eq!(cht1, cht2);
-        cht2.resize(3);
-        assert_eq!(cht1, cht2);
-        cht2.resize(2);
-        assert_eq!(cht1, cht2);
-        cht2.resize(1);
-        assert_eq!(cht1, cht2);
-    }
-
+    
     #[test]
     fn add() {
         let mut cht1 = ChainedHashTable {
             dim: 2,
             table: vec![vec![]; 4],
-            odd: 2799304215,
+            hasher: hashers::Multiplicative::with_seed(3141592),
             len: 0,
         };
         let out1 = cht1.add(42);
         let out2 = cht1.add(101);
         let out3 = cht1.add(0);
-        let out4 = cht1.add(58008);
+        let out4 = cht1.add(18446744073709551615);
         let out5 = cht1.add(0);
 
         assert_eq!(out1, Ok(()));
@@ -279,10 +214,10 @@ mod tests {
         let cht2 = ChainedHashTable {
             dim: 1,
             table: vec![
-                vec![42, 101, 0, 58008],
-                vec![],
+                vec![0, 101],
+                vec![42, 18446744073709551615],
             ],
-            odd: 1,
+            hasher: hashers::Multiplicative::with_seed(10101),
             len: 4,
         };
         assert_eq!(cht1, cht2);
@@ -293,30 +228,87 @@ mod tests {
         let mut cht1 = ChainedHashTable {
             dim: 2,
             table: vec![
+                vec![42, 101, 0],
                 vec![],
-                vec![1671459500],
-                vec![2712514241, 2618448044],
-                vec![2291933903],
+                vec![],
+                vec![18446744073709551615],
             ],
-            odd: 237597269,
+            hasher: hashers::Multiplicative::with_seed(8086),
             len: 4,
         };
-        let out1 = cht1.remove(1671459500);
+        let out1 = cht1.remove(42);
         assert_eq!(out1, Ok(()));
-        let out2 = cht1.remove(1671459500);
+        let out2 = cht1.remove(69);
         assert_eq!(out2, Err(Error::KeyNotFound));
-        let out3 = cht1.remove(2712514241);
+        let out3 = cht1.remove(101);
         assert_eq!(out3, Ok(()));
 
         let cht2 = ChainedHashTable {
             dim: 1,
             table: vec![
-                vec![2618448044],
-                vec![2291933903],
+                vec![0, 18446744073709551615],
+                vec![],
             ],
-            odd: 3296597657,
+            hasher: hashers::Multiplicative::with_seed(234),
             len: 2,
         };
         assert_eq!(cht1, cht2);
+    }
+
+    #[test]
+    fn contains() {
+        let cht1 = ChainedHashTable { 
+            dim: 2,
+            table: vec![
+                vec![0],
+                vec![18446744073709551615],
+                vec![24503201026],
+                vec![19850596],
+            ],
+            hasher: hashers::Multiplicative::with_seed(80085),
+            len: 4,
+        };
+        assert!(cht1.contains(0));
+        assert!(cht1.contains(18446744073709551615));
+        assert!(cht1.contains(24503201026));
+        assert!(!cht1.contains(42))
+    }
+
+    #[test]
+    fn resize() {
+        let cht1 = ChainedHashTable {
+            dim: 1, 
+            table: vec![
+                vec![0, 3151, 3053, 1496],
+                vec![18446744073709551615, 4253, 4161, 3199, 3057],
+            ],
+            hasher: hashers::Multiplicative::with_seed(1986),
+            len: 9,
+        };
+        let mut cht2 = cht1.clone();
+
+        cht2.resize(2);
+        assert_eq!(cht1, cht2);
+        assert_eq!(cht2.table.len(), 4);
+
+        cht2.resize(3);
+        assert_eq!(cht1, cht2);
+        assert_eq!(cht2.table.len(), 8);
+
+        cht2.resize(4);
+        assert_eq!(cht1, cht2);
+        assert_eq!(cht2.table.len(), 16);
+
+        cht2.resize(3);
+        assert_eq!(cht1, cht2);
+        assert_eq!(cht2.table.len(), 8);
+
+        cht2.resize(2);
+        assert_eq!(cht1, cht2);
+        assert_eq!(cht2.table.len(), 4);
+    
+        cht2.resize(1);
+        assert_eq!(cht1, cht2);
+        assert_eq!(cht2.table.len(), 2);
     }
 }
