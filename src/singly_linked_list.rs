f4b46862d43ef11837663ce4c8a83401f7db108e
Carlos Guzman <carlos.rpg@gmail.com>
2025-03-30 18:29:11 +0200

    Implement `SLList::pop()`

--- src/singly_linked_list.rs
+++ src/singly_linked_list.rs
@@ -39,6 +39,17 @@ impl<T> SLList<T> {
         self.size += 1;
     }
 
+    pub fn pop(&mut self) -> Option<T> {
+        let pop_link = Rc::clone(self.head.as_ref()?);
+        self.head = pop_link.next.as_ref().map(|link| Rc::clone(link));
+        self.size -= 1;
+
+        if self.head.is_none() {
+            self.tail = None;
+        }
+        Rc::into_inner(pop_link).map(|node| node.value)
+    }
+
     pub fn iter(&self) -> SLListIter<T> {
         SLListIter { ref_to: &self.head }
     }
@@ -119,5 +130,52 @@ mod tests {
         assert_eq!(list.size(), 2);
         list.push('c');
         assert_eq!(list.size(), 3);
+    }
+
+    #[test]
+    fn pop_from_initialized_returns_contents() {
+        let n1 = Rc::new(
+            Node { value: 1, next: None }
+        );
+        let n1_tail = Rc::clone(&n1);
+        let n2 = Rc::new(
+            Node { value: 2, next: Some(n1) }
+        );
+        let n3 = Rc::new(
+            Node { value: 3, next: Some(n2) }
+        );
+        let mut list = SLList {
+            head: Some(n3), tail: Some(n1_tail), size: 3,
+        };
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(2));
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), None);
+    }
+
+    #[test]
+    fn pop_from_initialized_keeps_track_of_size() {
+        let n1 = Rc::new(
+            Node { value: 1, next: None }
+        );
+        let n1_tail = Rc::clone(&n1);
+        let n2 = Rc::new(
+            Node { value: 2, next: Some(n1) }
+        );
+        let n3 = Rc::new(
+            Node { value: 3, next: Some(n2) }
+        );
+        let mut list = SLList {
+            head: Some(n3), tail: Some(n1_tail), size: 3,
+        };
+        assert_eq!(list.size(), 3);
+        list.pop();
+        assert_eq!(list.size(), 2);
+        list.pop();
+        assert_eq!(list.size(), 1);
+        list.pop();
+        assert_eq!(list.size(), 0);
+        list.pop();
+        assert_eq!(list.size(), 0);
     }
 }
