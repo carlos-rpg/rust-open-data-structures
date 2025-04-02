351e7f42db3449274b8bee9bbcb70338bbb7f96f
Carlos Guzman <carlos.rpg@gmail.com>
2025-04-02 23:07:14 +0200

    Include documentation

--- src/singly_linked_list.rs
+++ src/singly_linked_list.rs
@@ -1,8 +1,30 @@
+//! A safe singly linked list with head and tail access.
+
 use std::rc::Rc;
 use std::cell::RefCell;
 
 type Link<T> = Rc<RefCell<Node<T>>>;
 
+/// A safe singly linked list.
+/// 
+/// This implementation allows insertion at the head and the tail of the list 
+/// in constant time, but only extraction from the head is constant.
+/// 
+/// # Panics
+/// 
+/// Only anomalous states should trigger a panic, such as a head with more than 
+/// one shared owner or a head being Some(_) but not the tail and viceversa.
+///
+/// # Examples
+/// 
+/// ```
+/// # use ods::singly_linked_list::SLList;
+/// let mut list = SLList::new();
+/// list.push(1);
+/// list.add(-1);
+/// assert_eq!(list.pop(), Some(1));
+/// assert_eq!(list.pop(), Some(-1));
+/// ```
 pub struct SLList<T> {
     head: Option<Link<T>>,
     tail: Option<Link<T>>,
@@ -23,14 +45,42 @@ impl<T> Node<T> {
 }
 
 impl<T> SLList<T> {
+    /// Creates a new, empty list.
+    /// 
+    /// # Examples
+    /// 
+    /// ```
+    /// # use ods::singly_linked_list::SLList;
+    /// let list: SLList<bool> = SLList::new();
+    /// assert_eq!(list.size(), 0);
+    /// ```
     pub fn new() -> Self {
         Self { head: None, tail: None, size: 0 }
     }
 
+    /// Returns the number of elements currently in the list.
+    /// 
+    /// # Examples
+    /// 
+    /// ```
+    /// # use ods::singly_linked_list::SLList;
+    /// let mut list = SLList::new();
+    /// list.push('a');
+    /// assert_eq!(list.size(), 1);
+    /// ```
     pub fn size(&self) -> usize {
         self.size
     }
 
+    /// Inserts an element at the head of the list.
+    /// 
+    /// # Examples
+    /// 
+    /// ```
+    /// # use ods::singly_linked_list::SLList;
+    /// let mut list = SLList::new();
+    /// list.push(0);
+    /// ```
     pub fn push(&mut self, x: T) {
         let new_link = Node::new(x, self.head.as_ref());
         self.head = Some(Rc::clone(&new_link));
@@ -41,6 +91,22 @@ impl<T> SLList<T> {
         self.size += 1;
     }
 
+    /// Extracts the element at the head of the list.
+    /// 
+    /// # Panics
+    /// 
+    /// If the head of the list had a strong count for shared ownership greater 
+    /// than one, this would panic. This is however something that under a 
+    /// correct implementation should never happen.
+    /// 
+    /// # Examples
+    /// 
+    /// ```
+    /// # use ods::singly_linked_list::SLList;
+    /// let mut list = SLList::new();
+    /// list.push(0);
+    /// assert_eq!(list.pop(), Some(0));
+    /// ```
     pub fn pop(&mut self) -> Option<T> {
         let pop_link = Rc::clone(self.head.as_ref()?);
         self.head = pop_link.borrow_mut().next.take();
@@ -56,6 +122,21 @@ impl<T> SLList<T> {
         Some(pop_contents.value)
     }
 
+    /// Inserts an element at the tail of the list.
+    /// 
+    /// # Panics
+    /// 
+    /// If the head is `Some(_)` but the tail is `None`, the function panics. 
+    /// But this should never happen under a correct implementation.
+    /// 
+    /// # Examples
+    /// 
+    /// ```
+    /// # use ods::singly_linked_list::SLList;
+    /// let mut list = SLList::new();
+    /// list.add(10);
+    /// assert_eq!(list.pop(), Some(10));
+    /// ```
     pub fn add(&mut self, x: T) {
         let new_link = Node::new(x, None);
 
@@ -74,6 +155,20 @@ impl<T> SLList<T> {
         self.size += 1;
     }
 
+    /// Creates a consuming iterator, that is, one that moves each value out of
+    /// the list (from start to end). The list cannot be used after calling
+    /// this.
+    ///
+    /// # Examples
+    /// 
+    /// ```
+    /// # use ods::singly_linked_list::SLList;
+    /// let mut list = SLList::new();
+    /// list.push(2);
+    /// list.push(1);
+    /// list.push(0);
+    /// assert_eq!(list.into_iter().collect::<Vec<i32>>(), [0, 1, 2]);
+    /// ```
     pub fn into_iter(self) -> IntoIter<T> {
         IntoIter(self)
     }
@@ -88,6 +183,11 @@ impl<T> Iterator for IntoIter<T> {
     }
 }
 
+/// Iterative implementation of list desctruction.
+/// 
+/// The reason for this custom implementation is that the default one is 
+/// recursive, which has the risk of blowing the stack if the list is large 
+/// enough.
 impl<T> Drop for SLList<T> {
     fn drop(&mut self) {
         let mut next = self.head.take();
