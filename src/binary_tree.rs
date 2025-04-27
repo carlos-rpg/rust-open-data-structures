797042aef4a133a17346a2db73dee1707ec253a2
Carlos Guzman <carlos.rpg@gmail.com>
2025-04-27 23:22:17 +0200

    Replace `Rc<RefCell<Node<T>>>` for `Link<T>` everywhere

--- src/binary_tree.rs
+++ src/binary_tree.rs
@@ -1,57 +1,78 @@
 use std::rc::{Rc, Weak};
-use std::cell::RefCell;
+use std::cell::{Ref, RefCell, RefMut};
 
-
-pub struct Link<T>(Rc<RefCell<Node<T>>>);
 
 pub struct Node<T> {
     value: T,
-    parent: Weak<RefCell<Node<T>>>,
-    left: Option<Rc<RefCell<Node<T>>>>,
-    right: Option<Rc<RefCell<Node<T>>>>,
+    parent: WeakLink<T>,
+    left: Option<Link<T>>,
+    right: Option<Link<T>>,
 }
 
 impl<T> Node<T> {
     pub fn new(value: T) -> Node<T> {
-        Self { value, parent: Weak::new(), left: None, right: None }
+        Self { value, parent: WeakLink(Weak::new()), left: None, right: None }
     }
 }
 
 
+pub struct WeakLink<T>(Weak<RefCell<Node<T>>>);
+
+impl<T> WeakLink<T> {
+    pub fn upgrade(&self) -> Option<Link<T>> {
+        self.0.upgrade().map(|x| Link(x))
+    }
+}
+
+
+pub struct Link<T>(Rc<RefCell<Node<T>>>);
+
 impl<T> Link<T> {
     pub fn new(value: T) -> Self {
-        Self(Rc::new(RefCell::new(Node::new(value))))
+        Self( Rc::new(RefCell::new(Node::new(value))) )
+    }
+
+    pub fn borrow(&self) -> Ref<Node<T>> {
+        self.0.borrow()
+    }
+
+    pub fn borrow_mut(&self) -> RefMut<Node<T>> {
+        self.0.borrow_mut()
+    }
+
+    pub fn downgrade(&self) -> WeakLink<T> {
+        WeakLink(Rc::downgrade(&self.0))
     }
 
     pub fn depth(&self) -> usize {
         let mut depth = 0;
-        let mut link_opt = self.0.borrow().parent.upgrade();
+        let mut link_opt = self.borrow().parent.upgrade();
 
-        while let Some(parent_link) = link_opt {
+        while let Some(link) = link_opt {
             depth += 1;
-            link_opt = parent_link.borrow().parent.upgrade();
+            link_opt = link.borrow().parent.upgrade();
         }
         depth
     }
 
     pub fn size(&self) -> usize {
         let mut size = 0;
-        let mut links = vec![Rc::clone(&self.0)];
+        let mut links = vec![Link::clone(&self)];
         while !links.is_empty() {
             size += 1;
             let link = links.remove(0);
             if let Some(left_link) = &link.borrow().left {
-                links.push(Rc::clone(left_link));
+                links.push(Link::clone(left_link));
             }
             if let Some(right_link) = &link.borrow().right {
-                links.push(Rc::clone(right_link));
+                links.push(Link::clone(right_link));
             }
         }
         size
     }
 
     pub fn height(&self) -> usize {
-        fn recurse<T>(link_opt: &Option<Rc<RefCell<Node<T>>>>) -> usize {
+        fn recurse<T>(link_opt: &Option<Link<T>>) -> usize {
             match link_opt {
                 None => 0,
                 Some(link) => 1 + usize::max(
@@ -67,16 +88,22 @@ impl<T> Link<T> {
     }
 }
 
+impl<T> Clone for Link<T> {
+    fn clone(&self) -> Self {
+        Link(Rc::clone(&self.0))
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use std::collections::HashMap;
 
-    fn build_test_node<T>(value: T) -> Rc<RefCell<Node<T>>> {
-        Rc::new(RefCell::new(
-            Node { value, parent: Weak::new(), left: None, right: None }
-        ))
+    fn build_test_node<T>(value: T) -> Link<T> {
+        Link(Rc::new(RefCell::new(
+            Node { value, parent: WeakLink(Weak::new()), left: None, right: None }
+        )))
     }
 
     fn build_test_tree() -> HashMap<String, Link<i32>> {
@@ -87,28 +114,28 @@ mod tests {
         let rll = build_test_node(4);
         let rlr = build_test_node(5);
 
-        root.borrow_mut().left.replace(Rc::clone(&l));
-        root.borrow_mut().right.replace(Rc::clone(&r));
+        root.borrow_mut().left.replace(Link::clone(&l));
+        root.borrow_mut().right.replace(Link::clone(&r));
 
-        l.borrow_mut().parent = Rc::downgrade(&root);
+        l.borrow_mut().parent = Link::downgrade(&root);
 
-        r.borrow_mut().parent = Rc::downgrade(&root);
-        r.borrow_mut().left.replace(Rc::clone(&rl));
+        r.borrow_mut().parent = Link::downgrade(&root);
+        r.borrow_mut().left.replace(Link::clone(&rl));
 
-        rl.borrow_mut().parent = Rc::downgrade(&r);
-        rl.borrow_mut().left.replace(Rc::clone(&rll));
-        rl.borrow_mut().right.replace(Rc::clone(&rlr));
+        rl.borrow_mut().parent = Link::downgrade(&r);
+        rl.borrow_mut().left.replace(Link::clone(&rll));
+        rl.borrow_mut().right.replace(Link::clone(&rlr));
 
-        rll.borrow_mut().parent = Rc::downgrade(&rl);
-        rlr.borrow_mut().parent = Rc::downgrade(&rl);
+        rll.borrow_mut().parent = Link::downgrade(&rl);
+        rlr.borrow_mut().parent = Link::downgrade(&rl);
 
         let mut links = HashMap::new();
-        links.insert(String::from(""), Link(root));
-        links.insert(String::from("R"), Link(r));
-        links.insert(String::from("L"), Link(l));
-        links.insert(String::from("RL"), Link(rl));
-        links.insert(String::from("RLL"), Link(rll));
-        links.insert(String::from("RLR"), Link(rlr));
+        links.insert(String::from(""), root);
+        links.insert(String::from("R"), r);
+        links.insert(String::from("L"), l);
+        links.insert(String::from("RL"), rl);
+        links.insert(String::from("RLL"), rll);
+        links.insert(String::from("RLR"), rlr);
 
         links
     }
